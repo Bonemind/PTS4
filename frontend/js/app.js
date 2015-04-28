9925bfc554a81eb2b1f8b4cf11f5a2d3abc3ff1b
bonemind <subhime@gmail.com>
2015-04-28 12:22:35 +0000

    Added a few routes, and multiple directives

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,6 +5,7 @@ var PTSApp = angular.module("PTSApp", [
 	"angular-loading-bar",
 	"restangular",
 	"angularModalService",
+	"chart.js",
 	"ui.bootstrap.showErrors",
 	"MessageCenterModule"
 	]).run(["$rootScope", "$injector", function($rootScope, $injector) {
@@ -15,8 +16,11 @@ var PTSApp = angular.module("PTSApp", [
 			{status: "DONE", text: "Done"}, 
 			{status: "ACCEPTED", text: "Accepted"}
 		];
+		$rootScope.StoryTypes = [
+	 		{type: "USER_STORY", text: "Story"},
+	 		{type: "DEFECT", text: "Defect"},
+		]
 		$injector.get("$http").defaults.transformRequest = function(data, headersGetter) {
-			console.log($rootScope.token);
 			if ($rootScope.token !== undefined) {
 				headersGetter()["X-Token"] = $rootScope.token;
 			}
@@ -29,6 +33,22 @@ var PTSApp = angular.module("PTSApp", [
 //Configure restangular
 PTSApp.config(["RestangularProvider", function(RestangularProvider) {
 	RestangularProvider.setBaseUrl("http://localhost:8182/");
+	RestangularProvider.setParentless(false);
+	//Make sure a delete request has an empty body
+	RestangularProvider.setRequestInterceptor(function(elem, operation) {
+	   if (operation === "remove") {
+	         return undefined;
+	   } 
+
+	   if (operation === "post" || operation === "put") {
+		 for (var prop in elem) {
+			 if (elem.hasOwnProperty(prop) && _.isDate(elem[prop])) {
+				 elem[prop] = elem[prop].toISOString().slice(0,10);
+			 }
+		 }
+	   }
+	   return elem;
+	});
 }]);
 
 //Define routes
@@ -43,15 +63,30 @@ PTSApp.config(["$routeProvider",
 		}).when("/register", {
 			templateUrl: "templates/register.html",
 			controller: "RegistrationController"
-		}).when("/test", {
-			templateUrl: "templates/test.html",
-			controller: "TestController"
+		}).when("/teams", {
+			templateUrl: "templates/TeamList.html",
+			controller: "TeamListController"
+		}).when("/teams/:id/projects", {
+		 	templateUrl: "templates/ProjectList.html",
+			controller: "ProjectListController"
+		}).when("/teams/:id/users", {
+		 	templateUrl: "templates/TeamView.html",
+			controller: "TeamViewController"
 		}).when("/stories", {
 			templateUrl: "templates/UserStoryList.html",
 			controller: "UserStoryListController"
+		}).when("/teams/:id/backlog", {
+		 	templateUrl: "templates/Backlog.html",
+		 	controller: "BacklogController"
+		}).when("/teams/:id/iterations", {
+		 	templateUrl: "templates/IterationList.html",
+			controller: "IterationListController"
 		}).when("/story/:id/tasks", {
 			templateUrl: "templates/TaskList.html",
 			controller: "TaskListController"
+		}).when("/teams/:id/taskboard", {
+		 	templateUrl: "templates/TaskBoard.html",
+			controller: "BacklogController"
 		});
 	}]);
 
@@ -125,6 +160,72 @@ angular.module("PTSApp").directive('ptsStatus', ["$rootScope",
 			}
 		}]);
 
+angular.module("PTSApp").directive('teamMember', ["$rootScope", 
+		function($rootScope) {
+
+			return  {
+				restrict: 'AEC',
+				scope: {
+					userid: "=userid",
+					teammembers: "=teammembers"
+				},
+				template: "{{ readable }}",
+				controller: function($scope, $element, $attrs, $location) {
+					var readable = "None";
+					$scope.teammembers.forEach(function(member) {
+						if (member.id == $scope.userid) {
+							readable = member.email;
+						}
+					});
+					$scope.readable = readable;
+				}
+			}
+		}]);
+/**
+ * Resolves a story type to the human readable variant
+ * usage: <pts-type type="sometype"></pts-type>
+ */
+angular.module("PTSApp").directive('ptsType', ["$rootScope", 
+		function($rootScope) {
+
+			return  {
+				restrict: 'AEC',
+				scope: {
+					type: "=type"
+				},
+				template: "{{ readable }}",
+				controller: function($scope, $element, $attrs, $location) {
+				 	console.log($scope.type);
+					var StoryTypes = $rootScope.StoryTypes;
+				 	console.log(StoryTypes);
+					var readable = "None";
+					StoryTypes.forEach(function(type) {
+						if (type.type == $scope.type) {
+							readable = type.text;
+						}
+					});
+					$scope.readable = readable;
+				}
+			}
+		}]);
+/**
+ * Resolves a story type to the human readable variant
+ * usage: <pts-type type="sometype"></pts-type>
+ */
+angular.module("PTSApp").directive('backlogStory', ["$rootScope", 
+		function($rootScope) {
+
+			return  {
+				restrict: 'AECM',
+				scope: {
+					story: "=story"
+				},
+				templateUrl: "templates/StoryDirectiveTemplate.html",
+				controller: function($scope, $element, $attrs, $location) {
+				}
+			}
+		}]);
+
 /**
  * Used to hide statusses depending on the role of a user
  * user is the current logged in user
@@ -141,4 +242,20 @@ function cleanupStatusList(user, statusList) {
 	 return status.status !== "ACCEPTED";
 	});
 	return copiedStatus;
+}
+
+function dateAdd(date, interval, units) {
+  var ret = new Date(date); //don't change original date
+  switch(interval.toLowerCase()) {
+    case 'year'   :  ret.setFullYear(ret.getFullYear() + units);  break;
+    case 'quarter':  ret.setMonth(ret.getMonth() + 3*units);  break;
+    case 'month'  :  ret.setMonth(ret.getMonth() + units);  break;
+    case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
+    case 'day'    :  ret.setDate(ret.getDate() + units);  break;
+    case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
+    case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
+    case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
+    default       :  ret = undefined;  break;
+  }
+  return ret;
 }
