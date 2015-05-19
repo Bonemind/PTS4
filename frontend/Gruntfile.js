968608b24d28d442ce098593861c844d3716cbe9
bonemind <subhime@gmail.com>
2015-05-19 11:17:43 +0000

    Added livereload support and grunt watch task

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,6 +1,7 @@
 	module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-serve');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.initConfig({
 		serve: {
 		    options: {
@@ -11,6 +12,21 @@
 			basic: {
 				src: ['js/controllers/*.js'],
 				dest: 'js/controllers.js'
+			}
+		},
+		watch: {
+			html: {
+				files: "templates/*",
+				options: {
+					livereload: true
+				}
+			},
+			scripts: {
+				files: "js/controllers/*.js",
+				tasks: "concat",
+				options: {
+					livereload: true
+				}
 			}
 		}
 	});
