74f85fed4fad8143343d764f63438360a932b58b
Michon van Dooren <michon1992@gmail.com>
2015-05-26 11:11:52 +0200

    Added settings for the host/port.

--- backend/src/main/java/com/proftaak/pts4/Main.java
+++ backend/src/main/java/com/proftaak/pts4/Main.java
@@ -2,21 +2,22 @@ package com.proftaak.pts4;
 
 import com.proftaak.pts4.database.DBUtils;
 import com.proftaak.pts4.rest.SwitchBoard;
+import com.proftaak.pts4.utils.PropertiesUtils;
 import org.glassfish.grizzly.http.server.HttpServer;
+
+import java.util.Properties;
 
 /**
  * Created by Michon on 2-3-2015
  */
 public class Main {
-
-    private static final int PORT = 8182;
-
     public static void main(String[] args) throws Exception {
         // Prepare the database
         DBUtils.init();
 
         // Create the server
-        HttpServer server = HttpServer.createSimpleServer("/", Main.PORT);
+        Properties props = PropertiesUtils.getProperties();
+        HttpServer server = HttpServer.createSimpleServer("/", props.getProperty("general.host"), Integer.parseInt(props.getProperty("general.port")));
 
         // Create the switchboard
         SwitchBoard switchBoard = new SwitchBoard();
